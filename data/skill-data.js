c62988252a531eb6ada682cefd36d04164282d34
mstevenson <mbstevenson96@gmail.com>
2022-09-20 21:47:21 -0400

    create data file

---
+++ data/skill-data.js
@@ -0,0 +1,10 @@
+const skills = [
+  {skill: "painting", rockstar: false},
+  {skill: "good music taste", rockstar: true},
+  {skill: "dog training", rockstar: true},
+  {skill: "surfing", rockstar: false},
+]
+
+export {
+  skills
+}
